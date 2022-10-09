172d96243c54c7740b2238f0699ec4b0aec903e2
David <n10512799@qut.edu.au>
2022-10-09 16:32:28 +1000

    Layout Test

--- manager.c
+++ manager.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Function declarations
+void AddSum(void *ptr); void Divide(void *ptr);
+
 int main(int argc, char **argv){
 
-    // TESTING PULL PUSH
-    // TESTING PUSH
+    
 
     return 0;
+}
+
+void AddSum(void *ptr){
+    // Write your code
 }

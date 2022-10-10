995b46c425f0c68bfe9b82c02fa7d754576e8991
David <n10512799@qut.edu.au>
2022-10-10 15:59:10 +1000

    Shared Memory Initialisation

--- simulator.c
+++ simulator.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "carpark.h"
+
+shared_carpark_t carpark;
+
 
 int main(int argc, char **argv){
 
-    
+    init_carpark(&carpark);
 
     return 0;
 }
+
+
+

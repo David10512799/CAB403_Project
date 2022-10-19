4c789f9e42ed71b385602019e510582d3e3ca9e6
David <n10512799@qut.edu.au>
2022-10-19 22:32:15 +1000

    Simulator Read in plates and sim car

--- simulator.c
+++ simulator.c
@@ -18,6 +18,13 @@
 
 extern int errno;
 
+
+typedef struct car_sim car_sim_t;
+struct car_sim
+{
+    carpark_t *carpark;
+    char *plate;
+};
 //Car Simulation
 
 //Declare function that builds the LPR (License plate reader)
@@ -60,78 +67,36 @@ bool init_carpark(shared_carpark_t* carpark);
 void init_carpark_values(carpark_t* park);
 
 
-void *sim(void *arg);
+void *sim_car(void *arg);
 
 void pause_for(int time);
 
-//Global Variables
-char plate_registry[100][7];
-char plate[PLATE_LENGTH];
-int rand_plate;
 
 
 
 int main(void){
 
+    // Generate Shared Memory
     shared_carpark_t carpark;
     
     if (!init_carpark(&carpark))
     {
         fprintf(stderr, "Error initialising shared memory: %s\n", strerror(errno));
     }
-
-    pthread_t simulation;
-    pthread_create(&simulation, NULL, sim, &carpark);
-
-    while(true){
-        sleep(2);
-    }
-
-
-    return EXIT_SUCCESS;
-}
-
-// create thread
-void *sim(void *arg)
-{  
-    carpark_t *data = (carpark_t *)arg;
+    
+    
+    // Generate Hash Table
     htab_t verified_cars;
-
     int buckets = 40;
-    char plate;
 
     if (!htab_init(&verified_cars, buckets))
     {
         printf("failed to initialise hash table\n");
     }
     htab_insert_plates(&verified_cars);
-    
-    for(;;)
-        {
-            ms_pause(1); //random number between 1 to 100;
-            car_t *car;
-            char *plate;
-            
-            do{
-            //generate_plate();
-            srand(time(NULL));
-            rand_plate = rand() % 100;
-            
-
-            car = htab_find(&verified_cars, plate);
-
-            printf("%s\n", car->plate);
-
-            if(car == NULL){
-                break;
-            }
-            }while(car->in_carpark);
 
 
-        }
- }
-
-char generate_plate(){
+    // Read Plates File
     FILE* input_file = fopen("plates.txt", "r");
 
     if (input_file == NULL)
@@ -139,23 +104,79 @@ char generate_plate(){
         fprintf(stderr, "Error: failed to open file %s", "plates.txt");
         exit(1);
     }
-
-    int i = 0;
+    int plates_count = 0;
+    char plate[PLATE_LENGTH];
     while( fscanf(input_file, "%s", plate) != EOF )
     {
-        sprintf(plate_registry[i], plate);
-        //printf("%s\n", plate_registry[i]);
-        i++;
+        plates_count++;
+    }
+
+    // Going back to start of file
+    fseek(input_file, 0, SEEK_SET);
+
+    char **plate_registry = calloc(plates_count, PLATE_LENGTH);
+
+    int i = 0;
+    while( fscanf(input_file, "%s", plate) != EOF)
+    {
+        plate_registry[i] = plate;
     }
 
     fclose(input_file);
 
-    return *plate_registry[rand_plate];
+
+    // Simulate cars while firealarms are off
+    while (!carpark.data->level[0].temperature.alarm) 
+    {
+        car_t *car;
+        char *key;
+
+        do {
+
+        srand(time(NULL));
+        key = plate_registry[20]; // random number between 0 and plate_count
+
+        car = htab_find(&verified_cars, key);
+
+        } while(car->in_carpark);      
+
+        pthread_t valid_sim;
+        car_sim_t sim;
+        sim.carpark = &carpark;
+        sim.plate = key;
+        pthread_create(&valid_sim, NULL, sim_car, &sim);
+
+        ms_pause(100);
+
+
+        char *invalid_plate = "123ABC";
+
+        pthread_t invalid_sim;
+        sim.plate = invalid_plate;
+        pthread_create(&invalid_sim, NULL, sim_car, &sim);
+
+        ms_pause(100); //random number between 1 to 100;
+    }    
+
+
+
+    return EXIT_SUCCESS;
 }
 
-void generate_car(){
+// create thread
+void *sim_car(void *arg)
+{  
+    car_sim_t *car = (car_sim_t *)arg;
+    carpark_t *carpark = car->carpark;
+    char *plate = car->plate;
 
+ 
+
+    return NULL;
 }
+
+
+
 
 bool init_carpark(shared_carpark_t* carpark) 
 {

0613a8d6af2e37aca71814cfeaf8baa9f4f760c1
Aron <n11049600@qut.edu.au>
2022-10-24 14:09:41 +1000

    Level lpr

--- simulator.c
+++ simulator.c
@@ -147,7 +147,7 @@ void *monitor_gate(void *arg)
         }
 
         // signal condition variable and unlock mutex
-        pthread_cond_signal(&gate->condition); // Signal manager it set to lowing or closed
+        pthread_cond_signal(&gate->condition); // Signal manager it set to lowering or closed
         pthread_cond_broadcast(&localGate); // Signal car to check boomgate status
         pthread_mutex_unlock(&gate->mutex);
     }
@@ -160,29 +160,58 @@ void start_car_simulation(char **plate_registry, shared_carpark_t carpark, htab_
     // Simulate cars while firealarms are off
     while (!carpark.data->level[0].temperature.alarm) 
     {   
+        
+        int gen_rand = rand() % 2; // Can be 0 or 1
         car_t *car;
         char *key;
-        
-        // Generate random valid plates until found one that is not being used
-        do {                
-            srand(time(NULL));
-            int plate_number;
-            plate_number = (rand() % (plate_count -1));
-            key = plate_registry[plate_number]; // random number between 0 and plate_count
-            car = htab_find(verified_cars, key);
+        char rand_plate[PLATE_LENGTH];
+        int rand_wait = (rand() % 99) + 1;
 
-        } while(car->in_carpark || node_find_name_array(car_list, key, ENTRIES) != NULL);
-        
-        pthread_t valid_sim;
-        pthread_create(&valid_sim, NULL, sim_car, key);
+        // generate either a valid or invalid plate and start simulating a car with it
+        switch (gen_rand){
+            case 0: // Valid plate            
+                // Generate random valid plates until found one that is not being used
+                do {                
+                    srand(time(NULL));
+                    int plate_number;
+                    plate_number = (rand() % (plate_count -1));
+                    key = plate_registry[plate_number]; // random number between 0 and plate_count
+                    car = htab_find(verified_cars, key);
 
-        ms_pause(10); // Random time between 1 and 100 
+                } while(car->in_carpark || node_find_name_array(car_list, key, ENTRIES) != NULL);
+                
+                pthread_t valid_sim;
+                pthread_create(&valid_sim, NULL, sim_car, key);
 
-        char *invalid_plate = "123ABC"; //random invalid plate
-        pthread_t invalid_sim;
-        pthread_create(&invalid_sim, NULL, sim_car, invalid_plate);
+                ms_pause(rand_wait); // Random time between 1 and 100             
+                break;
 
-        ms_pause(10); //random number between 1 to 100;
+            case 1: // Invalid plate
+                do
+                {
+                    srand(time(NULL));
+                    for (int i = 0; i < 3; i++)
+                    {
+                        rand_plate[i] = (rand() % 10) + 48;
+                    }
+                    for (int i = 3; i < 6; i++)
+                    {
+                        rand_plate[i] = (rand() % 26) + 65;
+                    }
+                    car = htab_find(verified_cars, rand_plate);
+                } while (node_find_name_array(car_list, key, ENTRIES) != NULL || car != NULL);
+                
+
+                pthread_t invalid_sim;
+                pthread_create(&invalid_sim, NULL, sim_car, rand_plate);
+
+                ms_pause(rand_wait); //random number between 1 to 100;
+                break;    
+
+            default:
+                break;
+        }
+
         // // break;
     }    
 }
@@ -193,7 +222,6 @@ void *sim_car(void *arg)
 
     int random_entry = rand() % ENTRIES;
     // int random_exit = rand() % EXITS;
-    // int random_level = rand() % LEVELS;
     
     // Add car to queue
     pthread_mutex_lock(&queueMutex);
@@ -262,7 +290,8 @@ void *sim_car(void *arg)
 
         return NULL;
     }
-    
+    printf("level value is %d\n", level);
+    // level -= 1;
     // Wait for boomgate to open
     // printf("%s waiting for boomgate to open\n", plate);
 
@@ -287,8 +316,12 @@ void *sim_car(void *arg)
     // Travel to level
     ms_pause(10); // could be after level lpr
 
-    // Trigger level LPR    
+    // Trigger level LPR  
 
+    pthread_mutex_lock(&carpark.data->level[level].LPR.mutex);
+    strcpy(carpark.data->level[level].LPR.plate, plate);
+    pthread_cond_signal(&carpark.data->level[level].LPR.condition);
+    pthread_mutex_unlock(&carpark.data->level[level].LPR.mutex);
  
     return NULL;
 }
